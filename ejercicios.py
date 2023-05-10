3a5a343c8d317cde55c06309645dfa985c346480
joryrod <joryrodriguez25@gmail.com>
2023-05-10 16:56:11 -0500

    primer repositorio

---
+++ ejercicios.py
@@ -0,0 +1,42 @@
+## Convenciones para nombrar una variable
+
+## Primera letra en minúscula.
+## No pueden comenzar por números (Error: Syntax error on token) .
+## Si está conformada por dos palabras, la primera de estas deberá estar en minúsula y la segunda en mayúscula o ambas separadas por guión bajo (camel case, underscore).
+## Evitar nombres de una sola letra, con excepción de variables temporales(i, j, m, n).
+## No deben comenzar con guión bajo _ o signo dólar $( no es obligatorio, java lo permite).
+## No deben comenzar con caracteres extraños */+- (Error: cannot be resolved to a variable).
+## Poner nombres descriptivos es incluso mejor que comentariar el código( mi criterio).
+
+## Ejemplos: animalSalvaje, animal_salvaje
+
+## Convenciones para nombrar una clase
+
+## Primera letra en mayúsculas.
+## Sustantivos y singular.
+## Si está compuesta por más de una palabra usar pascal case(cada palabra comienza con mayúsculas).
+## Nombre simples y descriptivos.
+## Usar palabras completas, evitar acrónimos y abreviaturas en caso de no ser representativos, por ejemplo URL.
+
+## Ejemplos: Animal, AnimalSalvaje
+
+## Convenciones para nombrar una interfaz.
+
+## Aplican las mismas reglas que para las clases
+## En algunos casos se adiciona la letra I al inicio del nombre, no es obligatorio.
+
+## Ejemplos: Felino, AnimalMamífero
+
+## Convenciones para nombrar un método
+
+## Deben comenzar con infinitivos
+## Notación camel case( primera palabra en minúsculas y las siguientes con la primera letra en mayúsculas)
+## Es común asociar el nombre del método con la acción que ejecuta (insertar(), eliminar(), actualizar())
+## Poner nombres descriptivos es incluso mejor que comentariar el código( mi criterio)
+
+## Ejemplos: obtenerPago(), armar(), insertarPago().
+
+## Convenciones para nombrar constantes:
+## Nombre con todas sus letra en mayúsculas y las palabras separadas por guión bajo.
+
+## Ejemplos: URL_ENDPOINT, SERVER_PORT, IP_ADDRESS.
---
+++ main.py
@@ -0,0 +1,21 @@
+## pasos para declarar una variable
+## 1 . darle un nombre al identificador(variable)
+## 2 . escribir el operador de asignacion(=)
+## 3 . escribir el dato a almacenar
+
+## Reglas para nombrar variables
+## 1 . Solo pueden tener los siguientes caracteres
+##      - Letras minusculas
+##      - Letras mayusculas
+##      - Digitos
+##      - Guiones bajos(_)
+## 2 . solo deben iniciar con letras o guion bajo no con dijitos
+## 3 . No pueden ser una palabra reservada del lenguaje
+
+## Ejemplos de variables validas
+
+## a="valido"              -> 3="invalido"              
+## a3="valido"             -> 3a="invalido"
+## a_b_c="valido"          -> for="invalido"
+## _abc="valido"           -> with="invalido"
+## _3a="valido"            -> 3_a="invalido"
