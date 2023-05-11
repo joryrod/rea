cb2f37e212ea568bbba6f29e0e208963ec217f79
jory <131999429+joryrod@users.noreply.github.com>
2023-05-11 11:05:23 -0500

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-## pasos para declarar una variable
+pasos para declarar una variable
+
 ## 1 . darle un nombre al identificador(variable)
 ## 2 . escribir el operador de asignacion(=)
 ## 3 . escribir el dato a almacenar
 
-## Reglas para nombrar variables
+Reglas para nombrar variables
+
 ## 1 . Solo pueden tener los siguientes caracteres
 ##      - Letras minusculas
 ##      - Letras mayusculas
@@ -12,7 +14,7 @@
 ## 2 . solo deben iniciar con letras o guion bajo no con dijitos
 ## 3 . No pueden ser una palabra reservada del lenguaje
 
-## Ejemplos de variables validas
+Ejemplos de variables validas
 
 ## a="valido"              -> 3="invalido"              
 ## a3="valido"             -> 3a="invalido"
